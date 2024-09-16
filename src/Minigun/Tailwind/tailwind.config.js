2969d092e13397f0ab809dc3a3d2b43653c2147c
Reilly Oldham <reilly.oldham@gmail.com>
2024-09-16 22:15:11 +1200

    Implements Tailwind support

---
+++ src/Minigun/Tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        '../**/*.cshtml'
+    ],
+    theme: {
+        extend: {
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography')
+    ],
+}
