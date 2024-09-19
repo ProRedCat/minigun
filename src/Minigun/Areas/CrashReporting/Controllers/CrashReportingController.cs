270abf3cebdd1d99c232ced0d96fcd058b865cac
Reilly Oldham <reilly.oldham@gmail.com>
2024-09-19 23:32:23 +1200

    Create the initial dropdown - still needs more styling

--- src/Minigun/Areas/CrashReporting/Controllers/CrashReportingController.cs
+++ src/Minigun/Areas/CrashReporting/Controllers/CrashReportingController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
 using Minigun.Areas.Home.Controllers;
+using Minigun.Services;
 
 namespace Minigun.Areas.CrashReporting.Controllers;
 
@@ -7,15 +9,19 @@ namespace Minigun.Areas.CrashReporting.Controllers;
 public class CrashReportingController : Controller
 {
     private readonly ILogger<CrashReportingController> _logger;
+    private readonly IRaygunApiService _raygunApiService;
 
-    public CrashReportingController(ILogger<CrashReportingController> logger)
+    public CrashReportingController(ILogger<CrashReportingController> logger, IRaygunApiService raygunApiService)
     {
         _logger = logger;
+        _raygunApiService = raygunApiService;
     }
 
     [HttpGet("/crashreporting")]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        ViewData["Applications"] = await _raygunApiService.ListApplicationsAsync(100);
+        Console.WriteLine(JsonSerializer.Serialize(ViewData["Applications"]));
         return View();
     }
 }
