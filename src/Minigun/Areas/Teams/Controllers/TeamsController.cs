b1fa846fe299e669f299ab9733c01ca4b9aa77ce
Reilly Oldham <reilly.oldham@gmail.com>
2024-09-19 20:15:09 +1200

    Teams area

---
+++ src/Minigun/Areas/Teams/Controllers/TeamsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Minigun.Areas.Teams.Controllers;
+
+[Area("Teams")]
+public class TeamsController : Controller
+{
+    private readonly ILogger<TeamsController> _logger;
+
+    public TeamsController(ILogger<TeamsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/teams")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
