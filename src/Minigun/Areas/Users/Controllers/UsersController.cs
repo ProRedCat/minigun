ba96ee7c1fd1fd563831af0416592c68a0f37250
Reilly Oldham <reilly.oldham@gmail.com>
2024-09-19 20:05:25 +1200

    Users area

---
+++ src/Minigun/Areas/Users/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Minigun.Areas.Users.Controllers;
+
+[Area("Users")]
+public class UsersController : Controller
+{
+    private readonly ILogger<UsersController> _logger;
+
+    public UsersController(ILogger<UsersController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/users")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
