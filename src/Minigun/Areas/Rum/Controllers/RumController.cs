cdb79e4270e4e6df3437d6a98ac04ed554e74954
Reilly Oldham <reilly.oldham@gmail.com>
2024-09-19 19:54:09 +1200

    Added rum area

---
+++ src/Minigun/Areas/Rum/Controllers/RumController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Minigun.Areas.Home.Controllers;
+
+namespace Minigun.Areas.Rum.Controllers;
+
+[Area("Rum")]
+public class RumController : Controller
+{
+    private readonly ILogger<RumController> _logger;
+
+    public RumController(ILogger<RumController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/rum")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
