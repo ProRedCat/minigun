709b5784473f367998a5e35eb67d015862719dbd
Reilly Oldham <reilly.oldham@gmail.com>
2024-09-19 20:20:10 +1200

    Settings area

---
+++ src/Minigun/Areas/Settings/Controllers/SettingsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Minigun.Areas.Settings.Controllers;
+
+[Area("Settings")]
+public class SettingsController : Controller
+{
+    private readonly ILogger<SettingsController> _logger;
+
+    public SettingsController(ILogger<SettingsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/settings")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
