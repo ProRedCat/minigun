8bd02431fc75ba02ad520d763cfdb50e8aa52371
Reilly Oldham <reilly.oldham@gmail.com>
2024-09-19 20:02:02 +1200

    Deployments area

---
+++ src/Minigun/Areas/Deployments/Controllers/DeploymentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Minigun.Areas.Deployments.Controllers;
+
+[Area("Deployments")]
+public class DeploymentsController : Controller
+{
+    private readonly ILogger<DeploymentsController> _logger;
+
+    public DeploymentsController(ILogger<DeploymentsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/deployments")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
